2222adee7cd81654aa3f243b9a9868f5b6cafb62
Collin Clarke <collinclarke@gmail.com>
2018-12-22 14:08:48 -0800

    Fix api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   omdb_api_key: '4652d68f',
-  apiUrl: 'https://movie-list-backend.herokuapp.com/'
+  apiUrl: 'https://movie-list-backend.herokuapp.com'
 };
